6bb4ce8e997973acead3203294e13a3fe2f81d32
Dempsey Blum <dempseyblum@gmail.com>
2024-11-05 16:12:27 +1100

    Add time library

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use serenity::model::channel::Message;
 use serenity::builder::CreateMessage;
 use serenity::prelude::*;
 use serenity::utils::MessageBuilder;
+use time::macros::offset;
+use time::OffsetDateTime;
+
 
 
 struct Handler;
@@ -13,13 +16,27 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, context: Context, msg: Message) {
+        // Sets this server's timezone (by default is GMT+11)
+        // Must be in the format !changeTimezone
+        if msg.content.starts_with("!changeTimezone ") {
+
+            let response = MessageBuilder::new()
+                .push("Your timezone is now set to ")
+                .push_bold_safe("TODO")
+                .build();
+
+            if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+
         // Sets a user to get direct messages as alerts
         // Must be in the format !alertMe
         if msg.content.starts_with("!alertMe ") {
 
             let response = MessageBuilder::new()
                 .push("I will alert ")
-                .push_bold_safe(&msg.author.name)
+                .mention(&msg.author)
                 .push("When it is time for an event.  [TODO]")
                 .build();
 
@@ -34,7 +51,7 @@ impl EventHandler for Handler {
             // TODO Check if they were already getting alerts. If not do nothing.
             let response = MessageBuilder::new()
                 .push("I will no longer alert ")
-                .push_bold_safe(&msg.author.name)
+                .mention(&msg.author)
                 .push("When it is time for an event.  [TODO]")
                 .build();
 
@@ -49,7 +66,7 @@ impl EventHandler for Handler {
 
             let response = MessageBuilder::new()
                 .push("User ")
-                .push_bold_safe(&msg.author.name)
+                .mention(&msg.author)
                 .push(" has added the new event: ")
                 .push_bold_safe(" TODO ")
                 .push(" for ")
@@ -63,21 +80,14 @@ impl EventHandler for Handler {
 
         // Ping bot
         if msg.content == "!ping" {
-            let channel = match msg.channel_id.to_channel(&context).await {
-                Ok(channel) => channel,
-                Err(why) => {
-                    println!("Error getting channel: {why:?}");
-
-                    return;
-                },
-            };
+            let now: OffsetDateTime = OffsetDateTime::now_utc();
+            // TODO fix so the time is generated with an offset already. Was oddly difficult.
 
             let response = MessageBuilder::new()
-                .push("User ")
-                .mention(&msg.author)
-                .push(" used the 'ping' command in the ")
-                .mention(&channel)
-                .push(" channel")
+                .push("It is currently ")
+                .push(now.to_offset(offset!(+11)).time().to_string())
+                .push(" | ")
+                .push(now.to_offset(offset!(+11)).date().to_string())
                 .build();
 
             if let Err(why) = msg.channel_id.say(&context.http, &response).await {
